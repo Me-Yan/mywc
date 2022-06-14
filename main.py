bbf363016aaba163f75799fb9fc76a28ab627552
Me-Yan <yanyanghongami@163.com>
2022-06-14 11:28:08 +0800

    11111111

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from database import Database
 """
 if __name__ == "__main__":
 
-    flag = 2    # 1:表示查询商品  2:表示抢购
+    flag = 1    # 1:表示查询商品  2:表示抢购
 
     gt = GetData()
     db = Database()
@@ -14,11 +14,11 @@ if __name__ == "__main__":
     gt.login()
 
     if flag == 1:
-        goods_list = gt.get_all_data([1, 9], list(range(0, 7)))
+        goods_list = gt.get_all_data([1, 9], list(range(0, 8)))
         sql = gt.process_data(goods_list)
         db.insert_goods_data(sql=sql)
     elif flag == 2:
         gt.get_user_gtime()
-        gt.visit_all_goods(1, [0, 17000], 2)    # 抢几幅、价格区间、延迟几秒
+        gt.visit_all_goods(1, [0, 30000], 0.8)    # 抢几幅、价格区间、延迟几秒
 
 
